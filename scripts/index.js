733cff93b5aea475c3eab65332626a6caed8f844
Aleksandr Tulupov <Dragon32700@gmail.com>
2022-11-21 16:55:56 +0300

    попытка сделать изображение на весь экран

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ const trashButton = page.querySelectorAll('.element__bin')
 const profile_info = page.querySelector('.profile__info');
 const elements = page.querySelector('.elements');
 const element = page.querySelectorAll('.element');
-const image = page.querySelectorAll('.buttonImage')
+const image = page.querySelector('.element__image')
 const fullImage = page.querySelector('.fullImage');
 
 button.addEventListener('click', function () {
@@ -88,12 +88,15 @@ function add_profile(nameValue, descriptionValue) {
 
     elementEl.querySelector('.element__title').textContent = nameValue;
     elementEl.querySelector('.element__image').src = descriptionValue;
-    elementEl.querySelector('.element__heart').addEventListener('click', function (element){
+    elementEl.querySelector('.element__heart').addEventListener('click', function (element) {
         element.target.classList.toggle('_active')
     });
-    elementEl.querySelector('.element__bin').addEventListener('click', function (element){
+    elementEl.querySelector('.element__bin').addEventListener('click', function (element) {
         element.target.parentNode.remove();
     });
+    Array.from(image).forEach(function (element) {
+        element.addEventListener('click', openImageForm);
+    })
     elements.prepend(elementEl);
     console.log(elements)
 }
@@ -118,16 +121,18 @@ function likeElement(event) {
     }
 }
 
-Array.from(image).forEach(function (element) {
+Array.from(image).forEach(function (element) {  
     element.addEventListener('click', openImageForm);
 })
 
-imageCloseButton.addEventListener('click', function () {
-    openImageForm
-})
+imageCloseButton.addEventListener('click', openImageForm)
 
-function openImageForm(event) {
-    if (event.target.classList.contains('element__heart')) {
-        event.currentTarget.classList.toggle('_active');
-    }
+function openImageForm() {
+    overlay.classList.toggle('_visible');
+    fullImage.classList.toggle('_visible');
+    const imageFull = document.getElementById('imageFull');
+    const title = document.getElementById('titleFull');
+
+    title.textContent = title.value;
+    imageFull.src = image.value;
 }
